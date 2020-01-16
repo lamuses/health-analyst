ec6a3741b7a270dca816ea78088ab5a254db2447
lamus <lamuses@outlook.com>
2020-01-16 16:58:29 +0800

    Export genderParse and Scope

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import { BMI } from './bmi/bmi'
 import { BodyFat } from './bodyFat/bodyFat'
 import { HealthAnalyst } from './service/HealthAnalyst'
+import { genderParse } from './gender/genderParse'
+import { Scope } from './assets/enums/Scope'
 
 export {
   BMI,
   BodyFat,
   HealthAnalyst,
+  genderParse,
+  Scope
 }
